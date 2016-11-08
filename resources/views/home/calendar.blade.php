6cabbd9b27811caa53acaa8a61ca5e2c35e67d22
shawndibble <shawndibble@gmail.com>
2016-11-08 14:57:39 -0600

    re-arrange admin dashboard.

---
+++ resources/views/home/calendar.blade.php
@@ -0,0 +1,42 @@
+<ul class="collection with-header calendar z-depth-1">
+    <li class="collection-header">Calendar</li>
+    @foreach($calendar as $cal)
+        <li class="collection-item">
+            <div class="date white-text {{ $cal['date'] == \Carbon\Carbon::today()->format('Y-m-d') ? 'green darken-1' : 'indigo' }}">
+                <div class="month">
+                    {{ Carbon\Carbon::createFromFormat('Y-m-d', $cal['date'])->format('M') }}
+                </div>
+                <div class="day">
+                    {{ Carbon\Carbon::createFromFormat('Y-m-d', $cal['date'])->format('j') }}
+                </div>
+            </div>
+            <div class="events">
+                @if(count($cal['separated']) > 0)
+                    <div class="entry">
+                        The following records will be deleted: {!! implode("; ", array_map(function($a) {return "<a href='". url('user', $a['id']) . "'>". $a['last_name'] . ", ". $a['first_name'] ."</a>";}, $cal['separated'])) !!}
+                    </div>
+                @endif
+                @foreach($cal['travel'] as $travelCalendar)
+                    @if($travelCalendar->leave_date == $cal['date'])
+                        <div class="entry"> <a href="{{ url('user', $travelCalendar->user_id) }}">{{ $travelCalendar->user->userFullName }}</a> leaves for {{ $travelCalendar->location }}.</div>
+                    @else
+                        <div class="entry"> <a href="{{ url('user', $travelCalendar->user_id) }}">{{ $travelCalendar->user->userFullName }}</a> returns from {{ $travelCalendar->location }}.</div>
+                    @endif
+                @endforeach
+                @foreach($cal['trainingUser'] as $trainingUserCalendar)
+
+                    <div class="entry"> <a href="{{ url('training', array_values($trainingUserCalendar)[0]->training_id) }}">{{ array_values($trainingUserCalendar)[0]->training->name }}</a> is due for
+                        @if(count($trainingUserCalendar) > 3)
+                            {{ count($trainingUserCalendar) }} people.
+                        @else
+                            {!! implode("; ", array_map(function($a) {return $a['userLink'];}, $trainingUserCalendar)) !!}
+                        @endif
+                    </div>
+                @endforeach
+                @foreach($cal['newUser'] as $newUserCalendar)
+                    <div class="entry"> <a href="{{ url('user', $newUserCalendar->id) }}">{{ $newUserCalendar->userFullName }}'s</a> account was created.</div>
+                @endforeach
+            </div>
+        </li>
+    @endforeach
+</ul>
