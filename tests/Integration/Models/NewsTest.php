4ca372ff025f69d1fc5e151e0ee9f7370216f5f8
Sheela Keta <sketa@teamscci.com>
2016-11-18 09:58:33 -0600

    Minor Code Formatting

--- tests/Integration/Models/NewsTest.php
+++ tests/Integration/Models/NewsTest.php
@@ -15,10 +15,10 @@ class NewsTest extends TestCase
     public function get_published_news()
     {
         // Create a news
-    $createdNews = factory(SET\News::class)->create(['publish_date' => Carbon::now()->format('Y-m-d')]);
+        $createdNews = factory(SET\News::class)->create(['publish_date' => Carbon::now()->format('Y-m-d')]);
 
-    // Query the database using the scopePublishedNews method in the news model
-    $newsPublished = News::publishedNews()->where('id', $createdNews->id)->get();
+        // Query the database using the scopePublishedNews method in the news model
+        $newsPublished = News::publishedNews()->where('id', $createdNews->id)->get();
 
         // Assert that the correct news is returned
         $this->assertEquals($newsPublished->first()->title, $createdNews->title);
@@ -37,10 +37,10 @@ class NewsTest extends TestCase
                         ['publish_date' => Carbon::tomorrow()->format('Y-m-d')]); //,
                         // 'expire_date'=>Carbon::yesterday()->format('Y-m-d')]);
 
-    // Query the database using the scopePublishedNews method in the news model
-    $newsPublished = News::publishedNews()->where('id', $createdNews1->id)->get();
+        // Query the database using the scopePublishedNews method in the news model
+        $newsPublished = News::publishedNews()->where('id', $createdNews1->id)->get();
 
-    // Ensure that the query returns an empty collection
+        // Ensure that the query returns an empty collection
         $this->assertNotContains($createdNews1->title, $newsPublished->pluck('title'));
 
         $createdNews2 = factory(SET\News::class)->create(
@@ -48,9 +48,9 @@ class NewsTest extends TestCase
                          'expire_date'  => Carbon::yesterday()->format('Y-m-d'), ]);
 
         // Query the database using the scopePublishedNews method in the news model
-    $newsPublished = News::publishedNews()->where('id', $createdNews2->id)->get();
+        $newsPublished = News::publishedNews()->where('id', $createdNews2->id)->get();
 
-    // Ensure that the query returns an empty collection
+        // Ensure that the query returns an empty collection
         $this->assertNotContains($createdNews2->title, $newsPublished->pluck('title'));
     }
 }
