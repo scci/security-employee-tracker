30e63c7d5bdffc3c869257a0ecc92ad818aef3fd
shawndibble <shawndibble@gmail.com>
2016-12-09 10:45:16 -0600

    load up app_name only if we are loading the master layout.

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace SET\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use SET\Duty;
@@ -17,7 +18,9 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('app_name', Setting::get('app_name', 'SET'));
+        view()->composer(['layouts.master'], function ($view) {
+            $view->with('app_name', Setting::get('app_name', 'SET'));
+        });
 
         // Pass the logged in user's full name to the layouts._header view.
         view()->composer(['layouts._navbar', 'home._last_login'], function ($view) {
